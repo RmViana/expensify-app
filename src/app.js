253085c9328ffe413549c6639c51ec6c4001dc17
Romildo Alves <raj.alves96@gmail.com>
2020-10-13 00:01:30 -0300

    React-Router 101

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(
-  <p>This is my expensify app</p>,
-  document.getElementById("app")
+const ExpenseDashboardPage = () => <div>This from my dashboard component.</div>;
+
+const AddExpensePage = () => <div>This from my add expense component.</div>;
+
+const EditExpensePage = () => <div>This from my edit expense component.</div>;
+
+const HelpPage = () => <div>This from my help component.</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
 );
+
+ReactDOM.render(routes, document.getElementById("app"));
